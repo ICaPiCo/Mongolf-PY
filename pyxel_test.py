61cd6f5e38e68e15e627faf43647ace4c44990b6
ICaPiCo <icapico.ma@gmail.com>
2025-05-09 16:42:13 +0200

    Some changes

--- pyxel_test.py
+++ pyxel_test.py
@@ -1,4 +1,5 @@
 import pyxel
+from math import *
 
 screen_x,screen_y = 192, 120
 
@@ -7,6 +8,9 @@ class Balloon:
     yAxis=2
     rotation=0
     scale=0
+    letterSize=16
+    big=3
+    medium=2
 
     def __init__(self, name):
         self.name = name        
@@ -20,13 +24,13 @@ class Balloon:
         elif pyxel.btn(pyxel.KEY_S):
             self.yAxis+=0.1
         elif pyxel.btn(pyxel.KEY_Q):
-            self.xAxis+=0.1
-        elif pyxel.btn(pyxel.KEY_D):
             self.xAxis-=0.1
+        elif pyxel.btn(pyxel.KEY_D):
+            self.xAxis+=0.1
         elif pyxel.btn(pyxel.KEY_A):
-            self.rotation+=1
-        elif pyxel.btn(pyxel.KEY_E):
             self.rotation-=1
+        elif pyxel.btn(pyxel.KEY_E):
+            self.rotation+=1
         elif pyxel.btn(pyxel.KEY_UP):
             self.scale+=0.1
         elif pyxel.btn(pyxel.KEY_DOWN):
@@ -40,49 +44,57 @@ class Balloon:
         imgY+=10
         pyxel.blt(imgX, imgY, (0), 0, 0, 8, 8, colkey=0, scale=self.scale+1, rotate=self.rotation)
 
-        for i in range(3):
+        for i in range(3*self.big):
             pyxel.blt(imgX, (imgY), (0), 8, 0, 8, 8, colkey=0, scale=self.scale+1, rotate=self.rotation)
-            imgY-=self.yAxis
-            imgX-=self.xAxis
+            imgY-=self.yAxis/self.big
+            imgX-=self.xAxis/self.big
 
         pyxel.blt(imgX, imgY, (0), 0, 8, 8, 8, colkey=0, scale=self.scale+1, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
         
-        for i in range(3):
+        for i in range(3*self.big):
             pyxel.blt(imgX, (imgY), (0), 8, 8, 8, 8, colkey=0, scale=self.scale+1, rotate=self.rotation)
-            imgY-=self.yAxis
-            imgX-=self.xAxis
+            imgY-=self.yAxis/self.big
+            imgX-=self.xAxis/self.big
 
         pyxel.blt(imgX, imgY, (0), 16, 8, 8, 8, colkey=0, scale=self.scale+1, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
 
-        pyxel.blt(imgX, imgY, (0), 24, 0, 8, 8, colkey=0, scale=self.scale+3, rotate=self.rotation)
+        pyxel.blt(imgX, imgY, (0), 24, 0, 8, 8, colkey=0, scale=self.scale+4, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
 
-        pyxel.blt(imgX, imgY, (0), 24, 8, 8, 8, colkey=0, scale=self.scale+3, rotate=self.rotation)
+        pyxel.blt(imgX, imgY, (0), 24, 8, 8, 8, colkey=0, scale=self.scale+4, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
 
-        pyxel.blt(imgX, imgY, (0), 32, 0, 8, 8, colkey=0, scale=self.scale+3, rotate=self.rotation)
+        pyxel.blt(imgX, imgY, (0), 32, 0, 8, 8, colkey=0, scale=self.scale+4, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
 
-        pyxel.blt(imgX, imgY, (0), 24, 8, 8, 8, colkey=0, scale=self.scale+3, rotate=self.rotation)
+        pyxel.blt(imgX, imgY, (0), 24, 8, 8, 8, colkey=0, scale=self.scale+4, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
 
-        pyxel.blt(imgX, imgY, (0), 24, 0, 8, 8, colkey=0, scale=self.scale+3, rotate=self.rotation)
+        pyxel.blt(imgX, imgY, (0), 24, 0, 8, 8, colkey=0, scale=self.scale+4, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
 
-        pyxel.blt(imgX, imgY, (0), 16, 8, 8, 8, colkey=0, scale=self.scale+3, rotate=self.rotation)
+        pyxel.blt(imgX, imgY, (0), 16, 8, 8, 8, colkey=0, scale=self.scale+4, rotate=self.rotation)
         imgY-=self.yAxis
         imgX-=self.xAxis
 
-        pyxel.text(0, 0, self.name, 7)
+        textRealScale=0.5
+
+        pyxel.blt(self.letterSize*0.5, self.letterSize*0.5, (0), 0, 16, 16, 16, colkey=0, scale=textRealScale, rotate=self.rotation)
+        pyxel.blt(self.letterSize*0.5*2, self.letterSize*0.5, (0), 16, 16, 16, 16, colkey=0, scale=textRealScale, rotate=self.rotation)
+        pyxel.blt(self.letterSize*0.5*3, self.letterSize*0.5, (0), 32, 16, 16, 16, colkey=0, scale=textRealScale, rotate=self.rotation)
+        pyxel.blt(self.letterSize*0.5*4, self.letterSize*0.5, (0), 48, 16, 16, 16, colkey=0, scale=textRealScale, rotate=self.rotation)
+        pyxel.blt(self.letterSize*0.5*5, self.letterSize*0.5, (0), 64, 16, 16, 16, colkey=0, scale=textRealScale, rotate=self.rotation)
+
+        pyxel.text(0,0,f"xAxis: {(trunc(self.xAxis*10))/10}, yAxis: {(trunc(self.yAxis*10))/10}, rotation: {self.rotation}, scale: {(trunc(self.scale*10))/10}",7)
 
     def getTextSize(self, text):
         return len(text)*4, 8
@@ -96,6 +108,6 @@ class Balloon:
 
 mongol = Balloon("mongol")
 
-pyxel.init(screen_x, screen_y, title="Balloon", display_scale=9)
+pyxel.init(screen_x, screen_y, title="MONGO", display_scale=9)
 pyxel.load("my_resource.pyxres")
 pyxel.run(mongol.update,mongol.draw)
